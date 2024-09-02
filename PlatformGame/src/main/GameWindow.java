37a229bc4825ef6a281aebde2b1383abb0652643
kenko-l <kenko.latchman@gmail.com>
2024-09-01 23:46:21 -0400

    Updated JFrame to appear centered.

--- PlatformGame/src/main/GameWindow.java
+++ PlatformGame/src/main/GameWindow.java
@@ -17,6 +17,7 @@ public class GameWindow {
 		jframe.setLocationRelativeTo(null);
 		jframe.setResizable(false);
 		jframe.pack();
+		jframe.setLocationRelativeTo(null);
 		jframe.setVisible(true);
 		jframe.addWindowFocusListener(new WindowFocusListener() {
 
